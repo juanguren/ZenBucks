c649f40a4cca0ed6a34fd85da97421ca975a83b9
juanguren <juan_fe_7@live.com>
2020-09-19 18:14:15 -0500

    Method for revealing chart

--- src/v2/v2.js
+++ src/v2/v2.js
@@ -1,6 +1,7 @@
 
 let searchForm = document.getElementById("send-ticker");
 let searchInput = document.getElementById("input-asset");
+let chart = document.getElementById("myChart");
 
 const key = "398aa778137209a478ab10906b372f4f";
 
@@ -19,9 +20,45 @@ getFinancialInfo = (query) =>{
     fetch(`https://financialmodelingprep.com/api/v3/quote/${query}?apikey=${key}`)
         .then(data => data.json())
         .then((res) =>{
-            console.log(...res);
+            openChart(res[0])
         })
 }
 
-// ! TODO: Integrate a chart API.
-// ? Analize this one: https://css-tricks.com/marketstack-a-market-data-api/
+const openChart = (stockData) =>{
+    new Chart(chart, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
