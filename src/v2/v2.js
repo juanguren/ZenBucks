95dc893d5afee0518f6827bb7f58c0e4f5b3b4c8
juanguren <juan_fe_7@live.com>
2020-10-04 21:28:13 -0500

    Added chart.js DOM element + minor modification in js file

--- src/v2/v2.js
+++ src/v2/v2.js
@@ -33,12 +33,7 @@ const openChart = (stockData) =>{ // ! Sort array in reverse order. All dates ar
                 label: '# of Votes',
                 data: stockData.historical.map(all => all.close),
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(255, 99, 132, 0.2)'
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
