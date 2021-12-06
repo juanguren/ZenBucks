68380df59379e50becefb6c978e05c20c44eaaa5
Juan Felipe <juan_fe_7@live.com>
2021-12-05 20:32:13 -0500

    Abstracted the chart's config to independant method |  Housekeeping

--- src/v2/v2.js
+++ src/v2/v2.js
@@ -2,13 +2,48 @@ let searchForm = document.getElementById('send-ticker');
 let searchInput = document.getElementById('input-asset');
 let chart = document.getElementById('myChart');
 
-const key = '398aa778137209a478ab10906b372f4f';
-const getToDate = () => {
-  return moment().format('YYYY-MM-DD');
-};
+const key = '398aa778137209a478ab10906b372f4f'; // TODO: Erase this funny sequence from here
+
+const getToDate = () => moment().format('YYYY-MM-DD');
 const getFromDate = () => {
   const date = '2020-11-01';
   return moment(date).format('YYYY-MM-DD');
+};
+
+const getChartConfig = (stockData, tickerName) => {
+  return {
+    type: 'line',
+    data: {
+      labels: stockData.map((all) => all.date),
+      datasets: [
+        {
+          label: tickerName,
+          data: stockData.map((all) => all.close),
+          backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  };
 };
 
 const retrieveFinancials = async (ticker, target) => {
@@ -43,8 +78,7 @@ getFinancialInfo = async (ticker) => {
       const { symbol } = stockData;
       const getCompanyName = await retrieveFinancials(symbol, 'name');
       const { companyName } = getCompanyName[0];
-
-      openChart(stockData.data, companyName);
+      const newChart = openChart(stockData.data, companyName);
     } else {
       throw 'Unrecognized ticker symbol';
     }
@@ -56,39 +90,8 @@ getFinancialInfo = async (ticker) => {
 
 const openChart = (stockData, tickerName) => {
   try {
-    new Chart(chart, {
-      type: 'line',
-      data: {
-        labels: stockData.map((all) => all.date),
-        datasets: [
-          {
-            label: tickerName,
-            data: stockData.map((all) => all.close),
-            backgroundColor: ['rgba(255, 99, 132, 0.2)'],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-        },
-      },
-    });
+    const config = getChartConfig(stockData, tickerName);
+    return new Chart(chart, config);
   } catch (error) {
     console.log(error);
   }
